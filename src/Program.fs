d18876a67187dd6e4c06fffa0649d489fdbbae34
michel perez <michel.ingesoft@gmail.com>
2017-08-11 16:57:15 -0500

    wip

--- src/Program.fs
+++ src/Program.fs
@@ -3,8 +3,14 @@
 open System
 open Demo
 
+type Sum = Sum of int
+
 [<EntryPoint>]
+
 let main argv =
+  printfn "%A" argv
+  let sumin = Sum 1
+  printfn "S %A" sumin
   let numbers = [1; 2; 3]
   printfn "%A" (mapi [])
   printfn "%A" (mapi numbers)
@@ -13,4 +19,4 @@ let main argv =
   printf "Ingrese su nombre: "
   let name = Console.ReadLine ()
   printfn "hola mundo %s" name
-  0 // return an integer exit code
+  0 // return an integer exit code 
