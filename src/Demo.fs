c24ae2a682376bdb88e1a3c2c801d905512725db
Michel Perez <michel.ingesoft@gmail.com>
2017-08-10 23:27:24 -0500

    wip

---
+++ src/Demo.fs
@@ -0,0 +1,24 @@
+module Demo
+
+type Cmp =
+    | GT
+    | EQ
+    | LT
+
+type 'a Listi = 'a List
+
+let mapi (ls : int Listi) =
+  match ls with
+    | [] -> [-1]
+    | _ -> (List.map (fun x -> x + 1) ls)
+
+let reduci (ls : int Listi) =
+  ls
+  |> mapi
+  |> List.sum
+
+let pcmp cmp =
+  match cmp with
+    | GT -> printfn "Great"
+    | EQ -> printfn "Equal"
+    | LT -> printfn "Less"
---
+++ src/Program.fs
@@ -0,0 +1,16 @@
+﻿module Program
+
+open System
+open Demo
+
+[<EntryPoint>]
+let main argv =
+  let numbers = [1; 2; 3]
+  printfn "%A" (mapi [])
+  printfn "%A" (mapi numbers)
+  printfn "sum of: %d" (reduci numbers)
+  pcmp EQ
+  printf "Ingrese su nombre: "
+  let name = Console.ReadLine ()
+  printfn "hola mundo %s" name
+  0 // return an integer exit code
