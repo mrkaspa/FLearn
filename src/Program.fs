9dfec1919580960edd7ee496fc169997601bd68e
michel perez <michel.ingesoft@gmail.com>
2017-08-17 16:14:05 -0500

    w\ip

--- src/Program.fs
+++ src/Program.fs
@@ -1,19 +1,23 @@
 ﻿module Program
 
 open System
+open System.Threading
 open System.IO
 open Demo
 open XPrint
 open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.DotLiquid
+open DotLiquid
 
 type Sum = Sum of int
 
 let writeToFile (date: DateTime) filename text =
     let path = sprintf "%s-%O.txt" filename (date.ToString("yyMMdd"))
-    File.WriteAllText(path, text)    
+    File.WriteAllText(path, text)
 
-[<EntryPoint>]
-let main argv =
+let main1 argv =
     writeToFile DateTime.UtcNow.Date "log" "hola desde archivo"
     printfn "%A" argv
     let sumin = Sum 1
@@ -29,4 +33,25 @@ let main argv =
     printHello ()
     0 // return an integer exit code 
 
-// startWebServer defaultConfig (Successful.OK "Hello World!")
+type Model =
+  { title : string }
+
+setTemplatesDir "./../templates"
+
+let o = { title = "Hello World" }
+
+let app =
+  choose
+    [ GET >=> choose
+        [ path "/" >=> page "my_page.liquid" o ]]
+
+[<EntryPoint>]
+let main argv =
+    let cts = new CancellationTokenSource()
+    let conf = { defaultConfig with cancellationToken = cts.Token }
+    let listening, server = startWebServerAsync defaultConfig app
+    Async.Start(server, cts.Token)
+    printfn "Make requests now"
+    Console.ReadKey true |> ignore
+    cts.Cancel()
+    0
