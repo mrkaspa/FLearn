7a1cd22af563c10db6789d3f50727f3ee9ed407e
Michel Perez <michel.ingesoft@gmail.com>
2017-09-24 17:59:25 -0500

    wip

--- elmishdemo/src/Home/View.fs
+++ elmishdemo/src/Home/View.fs
@@ -22,12 +22,13 @@ let mdview _ _ =
 let root model dispatch =
     div []
         [ p [ ClassName "control" ]
-            [ input [ ClassName "input"
-                    ; Type "text"
-                    ; Placeholder "Type your name"; DefaultValue model
-                    ; AutoFocus true
-                    ; OnChange (fun ev -> !!ev.target?value |> ChangeStr |> dispatch )
-                    ]
+            [ input
+                [ ClassName "input"
+                ; Type "text"
+                ; Placeholder "Type your name"; DefaultValue model
+                ; AutoFocus true
+                ; OnChange (fun ev -> !!ev.target?value |> ChangeStr |> dispatch )
+                ]
             ]
         ; br []
         ; span [] [ str (sprintf "Hello %s" model) ]
