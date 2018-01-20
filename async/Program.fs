69937029b3dbfdb5771bb0b05c2105ad7fd5c634
Michel Perez <michel.ingesoft@gmail.com>
2018-01-20 14:08:27 -0500

    wip

--- async/Program.fs
+++ async/Program.fs
@@ -4,15 +4,26 @@ open System
 
 let random = Random()
 
-let pickNumber = async { return random.Next(10) }
+let pickNumber () = async { return random.Next(10) }
 
-[<EntryPoint>]
-let main _argv =
-    let workflows = [ for _i in 1..50 -> pickNumber ]
+let duration f =
+    let timer = Diagnostics.Stopwatch()
+    timer.Start()
+    let returnValue = f ()
+    printfn "Elapsed Time: %d" timer.ElapsedMilliseconds
+    returnValue
+
+let exec () =
+    let workflows = [ for _i in 1..50 -> pickNumber ()]
     let numbers =
         async {
             let! res = Async.Parallel workflows
             return List.ofArray res
         } |> Async.RunSynchronously
     printfn "Numbers = %A" numbers
+    ()
+
+[<EntryPoint>]
+let main _argv =
+    duration exec
     0 // return an integer exit code
