a34e8f805969092651f9aa0eb36a702ca2bfb838
Michel Perez <michel.ingesoft@gmail.com>
2018-04-26 10:28:09 -0500

    wip

--- async/Program.fs
+++ async/Program.fs
@@ -5,8 +5,8 @@ let random = Random()
 
 let pickNumberAsync () = async { return random.Next(10) }
 
-let pickNumberHopac resCh = job {
-    do! Ch.give resCh (random.Next(10))
+let pickNumberHopac () = job {
+    return random.Next(10)
 }
 
 let duration tag f =
@@ -23,19 +23,9 @@ let execAsync () =
     |> ignore
 
 let execHopac () =
-    let resCh = Ch<int>()
     [1..500]
-    |> List.map (fun _n -> pickNumberHopac resCh)
-    |> Job.conIgnore
-    |> start
-
-    job {
-        let acc = ref 0
-        for _i in [1..500] do
-            let! valu = Ch.take resCh
-            acc := !acc + valu
-        return !acc
-    }
+    |> List.map (fun _n -> pickNumberHopac ())
+    |> Job.conCollect
     |> run
     |> ignore
 
