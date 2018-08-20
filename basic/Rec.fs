31a753a5e6b532be0c40d7ae8558d068ca838178
Michel Perez <michel.ingesoft@gmail.com>
2018-08-20 15:47:24 -0500

    wip

--- basic/Rec.fs
+++ basic/Rec.fs
@@ -1,47 +1,47 @@
 module Rec
 
-module RecursiveModule =
+module rec RecursiveModule =
     type Orientation =
         | Up
         | Down
-    
+
     type PeelState =
         | Peeled
         | Unpeeled
-    
+
     // This exception depends on the type below.
     exception DontSqueezeTheBananaException of Banana
-    
+
     type BananaPeel() =
         class
         end
-    
+
     type Banana(orientation : Orientation) =
         member val IsPeeled = false with get, set
         member val Orientation = orientation with get, set
-        member val Sides : PeelState list = [ Unpeeled; Unpeeled; Unpeeled; 
+        member val Sides : PeelState list = [ Unpeeled; Unpeeled; Unpeeled;
                                               Unpeeled ] with get, set
         member self.Peel() = BananaHelpers.peel self // Note the dependency on the BananaHelpers module.
         member self.SqueezeJuiceOut() =
             raise (DontSqueezeTheBananaException self) // This member depends on the exception above.
-    
+
     module private BananaHelpers =
         let peel (b : Banana) =
             let flip (banana : Banana) =
                 match banana.Orientation with
-                | Up -> 
+                | Up ->
                     banana.Orientation <- Down
                     banana
                 | Down -> banana
-            
+
             let peelSides (banana : Banana) =
-                banana.Sides <- List.map (fun side -> 
+                banana.Sides <- List.map (fun side ->
                                     if side = Unpeeled then Peeled
                                     else side) banana.Sides
                 banana
-            
+
             match b.Orientation with
-            | Up -> 
+            | Up ->
                 b
                 |> flip
                 |> peelSides
